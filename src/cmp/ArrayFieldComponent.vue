676f17db8df152356ff30dedf24b83f93e962bb4
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2018-02-24 21:34:38 +0100

    objectField

--- src/cmp/ArrayFieldComponent.vue
+++ src/cmp/ArrayFieldComponent.vue
@@ -5,7 +5,6 @@
       <md-card-header-text>
         <div class="md-title" @click.native="toggleExpand">{{field.label}} ({{subItems.length}})</div>
         <div class="md-subhead" v-if="field.descr">{{field.descr}}</div>
-        {{subItems}}
       </md-card-header-text>
 
       <md-menu md-size="big" md-direction="bottom-end" @click="toggleExpand">
@@ -33,7 +32,7 @@
           <div class="md-layout-item md-size-100 md-layout md-alignment-center-left" v-if="expandedSubItem === i">
             <component
               v-bind:is="getFieldComponent(field.arrayOf)"
-              :field="field"
+              :field="getSubField()"
               :value="subItem"
               @input="updateSubItem($event, i)"
             />
@@ -44,6 +43,8 @@
           <md-button @click="onDone">Done</md-button>
         </md-card-actions>
       </md-card>
+
+      <pre>{{subItems}}</pre>
     </md-card-content>
 
     <md-card-actions md-alignment="left" v-show="expanded">
@@ -105,7 +106,7 @@ export default class ArrayFieldComponent extends BaseFieldComponent {
 
   addEmptySubItem () {
     const subItems = this.subItems
-    subItems.push('')
+    subItems.push('{}') // todo: !!!
     this.subItems = subItems
     this.expandedSubItem = subItems.length - 1
     console.log('exp: ' + this.expandedSubItem)
@@ -119,6 +120,19 @@ export default class ArrayFieldComponent extends BaseFieldComponent {
 
   getFieldComponent (type: string) {
     return schemaService.getFieldComponent(type)
+  }
+
+  getSubField () {
+    const subType = this.$store.getters.getTypeByName(this.field.arrayOf)
+    console.log('subType: ', subType)
+    if (subType) {
+      // for object types
+      subType.type = subType.name
+      return subType
+    }
+
+    // for primitive types
+    return this.field
   }
 
   updateSubItem (v: any, i: any) {
--- src/cmp/ObjectFieldComponent.vue
+++ src/cmp/ObjectFieldComponent.vue
@@ -1,35 +1,29 @@
 <template>
-  <md-card style="width: 100%; max-width: 600px; margin-bottom: 20px;">
-    <md-card-header>
+  <md-card :md-with-hover="!expanded"
+           style="width: 100%; max-width: 600px; margin-bottom: 20px; margin-left: 0; margin-right: 0;">
+    <md-card-header @click.native="toggleExpand">
       <md-card-header-text>
-        <div class="md-title">{{field.name}}</div>
-        <!--<div class="md-subhead">{{field.type}}</div>-->
+        <div class="md-title" @click.native="toggleExpand">{{field.label}}</div>
+        <div class="md-subhead" v-if="field.descr">{{field.descr}}</div>
       </md-card-header-text>
 
-      <md-menu md-size="big" md-direction="bottom-end">
-        <md-button class="md-icon-button" md-menu-trigger>
-          <md-icon>more_vert</md-icon>
+      <md-menu md-size="big" md-direction="bottom-end" @click="toggleExpand">
+        <md-button class="md-icon-button" @click="toggleExpand">
+          <md-icon>{{expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}}</md-icon>
         </md-button>
-
-        <md-menu-content>
-          <md-menu-item>
-            <span>Call</span>
-            <md-icon>phone</md-icon>
-          </md-menu-item>
-
-          <md-menu-item>
-            <span>Send a message</span>
-            <md-icon>message</md-icon>
-          </md-menu-item>
-        </md-menu-content>
       </md-menu>
     </md-card-header>
 
-    <md-card-content>
+    <md-card-content v-show="expanded">
       <component
         v-for="f in subType.fields" :key="f.name"
         v-bind:is="getFieldComponent(f.type)"
-        :field="f" :item="subItem"/>
+        :field="f"
+        :value="subItem[f.name]"
+        @input="updateSubItem($event, f.name)"
+        />
+
+      <pre>{{subItem}}</pre>
     </md-card-content>
   </md-card>
 </template>
@@ -41,19 +35,36 @@ import { BaseFieldComponent } from './BaseFieldComponent';
 
 @Component
 export default class ObjectFieldComponent extends BaseFieldComponent {
+  expanded = true
+
   get subType (): Field {
+    console.log('subType: ' + this.field!.type)
     return this.$store.getters.getTypeByName(this.field!.type)
   }
 
   get subItem (): any {
-    const o = this.item[this.field!.name]
-    if (!o) return {}
-    const subItem = JSON.parse(o)
-    return subItem
+    return JSON.parse(this.value || '{}')
+  }
+
+  set subItem (v: any) {
+    this.$emit('input', JSON.stringify(v || {}))
   }
 
   getFieldComponent (type: string) {
     return schemaService.getFieldComponent(type)
+  }
+
+  toggleExpand () {
+    this.expanded = !this.expanded
+  }
+
+  updateSubItem (v: any, fieldName: string) {
+    console.log('updateSubItem', v, fieldName)
+
+    this.subItem = {
+      ...this.subItem,
+      [fieldName]: v,
+    }
   }
 }
 </script>
